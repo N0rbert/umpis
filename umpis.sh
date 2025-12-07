c1030a7373da848d5ad091b3c0a32523fbc0a3ca
N0rbert <nrbrtx@gmail.com>
2025-12-07 17:55:22 +0300

    allow apt-xapian-index downgrade, add Arduino workaround

--- umpis.sh
+++ umpis.sh
@@ -337,7 +337,7 @@ EOF
     add-apt-repository -y ppa:nrbrtx/synaptic
   fi
 
-  apt-get install -y htop mc ncdu aptitude synaptic apt-xapian-index apt-file command-not-found
+  apt-get install -y htop mc ncdu aptitude synaptic apt-xapian-index apt-file command-not-found --allow-downgrades
 fi
 
 [ ! -e "/var/lib/synaptic/preferences" ] && mkdir -p /var/lib/synaptic/ && touch /var/lib/synaptic/preferences
@@ -1090,7 +1090,7 @@ apt-get autoremove -y
 if [[ "$ver" != "stretch" && "$ver" != "astra9" && "$ver" != "trusty" ]]; then
   if [ $is_docker == 0 ] ; then
     usermod -a -G dialout "$SUDO_USER"
-    sudo -u "$SUDO_USER" -- "$umake_path" electronics arduino-legacy
+    sudo -u "$SUDO_USER" -- "$umake_path" electronics arduino-legacy || sudo apt-get install -y arduino || echo "Error: unable to install Arduino Legacy 1.x IDE using neither umake nor APT, you have to choose other method of installation!"
   fi
 fi
 
